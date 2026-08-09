9e5be63ca18111615b692d6b59d92f9f2de90bae
boya16573-design <boya16573@gmail.com>
2026-08-10 00:05:06 +0530

    Refactor Supabase client initialization
    Signed-off-by: boya16573-design <boya16573@gmail.com>

--- supabase-client.js
+++ supabase-client.js
@@ -1,4 +1,4 @@
-window.CUBE3_CONFIG = {
-  SUPABASE_URL: "https://zjgtceurzsvjkspkvr...supabase.co",
-  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_q9x-cmieOqpj3_rw3AGcEA_K-uynMSm"
-};
+window.cube3Supabase = supabase.createClient(
+  window.CUBE3_CONFIG.SUPABASE_URL,
+  window.CUBE3_CONFIG.SUPABASE_PUBLISHABLE_KEY
+);
